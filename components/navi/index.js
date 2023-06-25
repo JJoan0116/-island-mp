0f36341d18fdac0d3818e138c141c5fbe62fee35
JJoan <1720152558@qq.com>
2023-06-25 15:12:20 +0800

    feat: add classic components

---
+++ components/navi/index.js
@@ -0,0 +1,26 @@
+// components/navi/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {},
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    latest: false,
+    disLeftSrc: '',
+    highLeftSrc: '',
+    title: '',
+    first: false,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLeft: () => {},
+    onRight: () => {},
+  },
+});
